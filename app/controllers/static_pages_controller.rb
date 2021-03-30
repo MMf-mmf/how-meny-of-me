ddbc6a971d01043dd52675abc7f41f405a9584f1
MenachemFuterfas <menachemfuterfas@gmail.com>
2021-03-30 16:36:12 -0400

    Add a Static Pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,37 @@
 class StaticPagesController < ApplicationController
   def home
+    # render :home
+  end
+
+  def scrape_search_results
+    agent = Mechanize.new
+
+    page = agent.get("http://howmanyofme.com/search/")
+    #p page.title
+
+    search_form = page.form("datain")
+    search_form.given = params[:fname]
+    search_form.sur = params[:lname]
+    
+    page = agent.submit(search_form, search_form.buttons.first)
+    #pp page
+
+    # num Of people with the same first name
+    @first_name_match = page.search('span.popnum')[0].text
+    # num Of  peaple with the same last name
+    @last_name_match = page.search('span.popnum')[1].text
+    # this will get the num of peaple with the same name
+    @full_name_match = page.search('span.popnum')[-1].text
+
+    # render scrape_search_results
   end
 
   def help
+  end
+
+  def about
+  end
+
+  def hello
   end
 end
