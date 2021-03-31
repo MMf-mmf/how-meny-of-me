17fb89f497c08df71a1510a12bd5b628d078e5cf
MenachemFuterfas <menachemfuterfas@gmail.com>
2021-03-31 02:12:10 -0400

    basic user/history model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    has_many :history
+
+    before_save { self.email = email.downcase }
+    validates :name, presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :name,  presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: true
+
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 4 }
+end
---
+++ db/migrate/20210331052430_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string "password_digest"
+      t.index ["email"], name: "index_users_on_email", unique: true
+
+      t.timestamps
+    end
+  end
+end
